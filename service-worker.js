c73611555e7d26bbf06341ce05c9d9fc9ee0bc96
Fidgetboyrolly <fidgetboyrolly@icloud.com>
2026-07-01 11:27:12 -0700

    Refactor service worker for improved caching
    Updated caching strategy to use versioned cache name and added activation event to clean up old caches.

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,29 @@
-self.addEventListener('install', e => {
+const CACHE = "loader-v2"; // bump this when you update index.html
+
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open('loader').then(cache => {
+    caches.open(CACHE).then(cache => {
       return cache.addAll([
-        './index.html',
-        './manifest.json'
+        "./index.html",
+        "./manifest.json"
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(resp => resp || fetch(e.request))
   );
